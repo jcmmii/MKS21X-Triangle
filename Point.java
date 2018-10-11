f9f303de98cd036412bf2fccfc7ff2599d640a9c
jcmmii <jchen06@stuy.edu>
2018-10-11 10:13:25 -0400

    Updated code

--- Point.java
+++ Point.java
@@ -23,40 +23,20 @@ public class Point{
     return y;
   }
 
-  public double distanceTo(Point ok) {
+  public double distanceTo(Point other) {
     double w = 0;
     double h = 0;
-    if (this.x > ok.x) {
-      w = this.x - ok.x;
-    }
-    if (this.x < ok.x) {
-      w = ok.x - this.x;
-    }
-    if (this.y > ok.y) {
-      h = this.y - ok.y;
-    }
-    if (this.y < ok.y) {
-      h = ok.y - this.y;
-    }
-    return Math.sqrt(w*w + h*h);
+    h = Math.pow(x - other.x, 2);
+    w = Math.pow(y - other.y, 2);
+    return Math.sqrt(h+w);
   }
 
   public static double distance(Point a, Point b) {
     double w = 0;
     double h = 0;
-    if (a.x > b.x) {
-      w = a.x - b.x;
-    }
-    if (a.x < b.x) {
-      w = b.x - a.x;
-    }
-    if (a.y > b.y) {
-      h = a.y - b.y;
-    }
-    if (a.y < b.y) {
-      h = b.y - a.y;
-    }
-    return Math.sqrt(w*w + h*h);
+    h = Math.pow(a.x - b.x, 2);
+    w = Math.pow(a.x - b.x, 2);
+    return Math.sqrt(w + h);
   }
 
   public String toString() {
