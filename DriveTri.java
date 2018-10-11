d42310936601875a9806aa2ff28cc47fadf61621
Justin Chen <jchen06@stuy.edu>
2018-10-10 21:37:53 -0400

    Working Driver

--- DriveTri.java
+++ DriveTri.java
@@ -3,17 +3,34 @@ public class DriveTri {
       Point P1 = new Point(0,0);
       Point P2 = new Point(0,3);
       Point P3 = new Point(4,0);
-      Triangle C = new Triangle(0,12,0,0,5,0);
-      Triangle D = new Triangle(P1,P2,P3);
-      System.out.println(C);
-    //  Triangle D = new Triangle(P1,P2,P3);
-    //  System.out.print(D);
-      System.out.println(C.getPerimeter());
-      System.out.println("Point 0: " + D.getVertex(0));
-      System.out.println("Point 1: " + D.getVertex(1));
-      D.setVertex(0,P2);
-      D.setVertex(1,P1);
-      System.out.println("Point 0: " + D.getVertex(0));
-      System.out.println("Point 1: " + D.getVertex(1));
+      Triangle A = new Triangle(P1,P2,P3);
+      //uses first method
+      Triangle B = new Triangle(0,12,0,0,5,0);
+      //uses second method
+
+      System.out.println("Triangle A: " + A);
+      //prints out Triangle A's vertices
+
+      System.out.println("Triangle B: " + B);
+      //prints out Triangle B's vertices
+
+      System.out.println("Triangle A's perimeter: " + A.getPerimeter());
+      //prints out Triangle A's perimeter
+
+      System.out.println("Triangle B's perimeter: " + B.getPerimeter());
+      //prints out Triangle B's perimeter
+
+      System.out.println("A, vertex 1: " + A.getVertex(0));
+      System.out.println("A, vertex 2: " + A.getVertex(1));
+      System.out.println("A, vertex 3: " + A.getVertex(2));
+      //prints out A's vertices individually
+
+      Point P4 = new Point (100,100);
+      A.setVertex(0,P4);
+      //changes vertex 1 from 0,0 to 100,100
+
+      System.out.println("A, new vertex 1: " + A.getVertex(0));
+      System.out.println("New Triangle A: " + A);
+      System.out.println("New Triangle A's perimeter: " + A.getPerimeter());
     }
   }
